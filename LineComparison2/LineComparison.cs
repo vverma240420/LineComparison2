ac26f0bc16e2d2807ad48c676420923e8b719b89
Vivek Verma <vverma240420@gmail.com>
2023-02-24 14:26:25 +0530

    Add Line Comparison

---
+++ LineComparison2/LineComparison.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison2
+{
+    internal class LineComparison
+    {
+        public static void LineCompare()
+        {
+            Console.WriteLine("Enter the value for cooridinate x1");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value for cooridinate y1");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value for cooridinate x2");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value for cooridinate y2");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value for cooridinate x3");
+            int x3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value for cooridinate y3");
+            int y3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value for cooridinate x4");
+            int x4 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value for cooridinate y4");
+            int y4 = Convert.ToInt32(Console.ReadLine());
+
+            double first = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            double second = Math.Sqrt(Math.Pow((x4 - x3), 2) + Math.Pow((y4 - y3), 2));
+            Console.WriteLine("Welcome to the Line Comparison Problems");
+            Console.WriteLine("Length of Line1:" + first);
+            Console.WriteLine("Length of Line2:" + second);
+
+            double compare = first.CompareTo(second);
+
+            if (compare >= 0)
+            {
+                Console.WriteLine("First line is greater than second line");
+            }
+            else
+            {
+                Console.WriteLine("Second line is greater than first line");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
